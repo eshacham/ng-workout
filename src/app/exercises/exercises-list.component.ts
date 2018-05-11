74cd309f131c2a19761a5d87d894ed87af101889
Eyal Shacham <shacham@swbell.net>
2018-05-10 19:26:09 -0500

    update data structure - use workout, days, exercises.

--- src/app/exercises/exercises-list.component.ts
+++ src/app/exercises/exercises-list.component.ts
@@ -6,44 +6,50 @@ import { Component } from '@angular/core';
 })
 export class ExercisesListComponent {
 
-    exercise1 = {
-        id: 1,
-        name: 'Bench Press, Wide Grip',
-        imageUrl: 'assets/images/CrossPressWideGrip.jpeg',
-        grip: {
-            type: 'Overhand',
-            width: 'Wide',
-        },
-        repetition: {
-            speed: '1:1'
-        },
-        weightType: 'Barbell',
-        isFavorite: false,
-        sets: [
-            {
-                weight: 95,
-                unit: 'lbs',
-                times: 12
+workout = {
+    name: 'ABC + Full Body Option 1 *',
+    days: [{
+        name: 'Full Body',
+        exercises: [{
+            id: 1,
+            name: 'Bench Press, Wide Grip',
+            imageUrl: 'assets/images/CrossPressWideGrip.jpeg',
+            grip: {
+                type: 'Overhand',
+                width: 'Wide',
             },
-            {
-                weight: 95,
-                unit: 'lbs',
-                times: 12
+            repetition: {
+                speed: '1:1'
             },
-            {
-                weight: 95,
-                unit: 'lbs',
-                times: 12
-            },
-            {
-                weight: 95,
-                unit: 'lbs',
-                times: 12
-            }
-        ],
-        restBetweenSets: 20,
-        restAfterExercise: 20
-    };
+            weightType: 'Barbell',
+            isFavorite: false,
+            sets: [
+                {
+                    weight: 95,
+                    unit: 'lbs',
+                    times: 12
+                },
+                {
+                    weight: 95,
+                    unit: 'lbs',
+                    times: 12
+                },
+                {
+                    weight: 95,
+                    unit: 'lbs',
+                    times: 12
+                },
+                {
+                    weight: 95,
+                    unit: 'lbs',
+                    times: 12
+                }
+            ],
+            restBetweenSets: 20,
+            restAfterExercise: 20
+        }]
+    }]
+};
 
     handleExerciseClick(data) {
         console.log('receieved: ', data);
