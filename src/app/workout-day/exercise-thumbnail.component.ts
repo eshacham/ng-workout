3fd6f80395aa43310eae7a2a60564c076e2c6460
Eyal Shacham <shacham@swbell.net>
2018-05-12 09:34:54 -0500

    update style for the exercise component

--- src/app/workout-day/exercise-thumbnail.component.ts
+++ src/app/workout-day/exercise-thumbnail.component.ts
@@ -3,9 +3,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
     selector: 'app-exercise-thumbnail',
     templateUrl: './exercise-thumbnail.component.html',
-    styles: [`
-        .img-sml { height: 100px; }
-    `]
+    styleUrls: ['./exercise-thumbnail.component.css']
 })
 export class ExerciseThumbnailComponent {
     @Input() exercise: any;
